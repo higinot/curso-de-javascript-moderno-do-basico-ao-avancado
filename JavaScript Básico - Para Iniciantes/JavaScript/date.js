88c877920f8879ba68b1a91d65cfaae6e433f3cc
higinot <engprodhigino@gmail.com>
2022-04-27 12:08:19 -0300

    Aprendendo sobre Objeto Date()

--- JavaScript Básico - Para Iniciantes/JavaScript/date.js
+++ JavaScript Básico - Para Iniciantes/JavaScript/date.js
@@ -1,2 +1,17 @@
-const data = new Date();
-console.log(data.toString())
+const tresHoras = 60 * 60 * 1000;
+
+
+
+const data1 = new Date(2018, 3, 20, 12, 15, 60); //Exata hora de agora (Ano, mẽs , dia, hora, minuto, segundo, ms)
+const data2 = new Date('2020-04-20 20:20:40')
+
+
+
+console.log(data2.toString())
+console.log('Dia', data2.getDate())
+console.log('Mês', data2.getMonth())
+console.log('Ano', data2.getFullYear())
+console.log('Hrs', data2.getHours())
+console.log('Min', data2.getMinutes())
+console.log('Segs', data2.getSeconds())
+console.log('Ms', data2.getMilliseconds())
